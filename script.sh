7d8d7cae1b8d3ad669a4eb153a991a2ce17af224
Yevhenii Kyrychenko <kyrychenko.yevhenii@gmail.com>
2018-07-05 11:22:38 +0300

    script.h fixed #2

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo yum install gcc -y
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 git clone https://github.com/dierz/nextid_repo.git /home/vagrant/nextid_repo
-mysql -uroot < /home/vagrant/nextid_repo/nextid_repo/dump.sql
+mysql -uroot < /home/vagrant/nextid_repo/dump.sql
 sudo cp /home/vagrant/nextid_repo/nextid.service /lib/systemd/system/
 sudo gcc /home/vagrant/nextid_repo/src/nextid_server.c -o /home/vagrant/nextid_repo/nextid_server `mysql_config --libs --include`
 sudo chmod +x /home/vagrant/nextid_repo/nextid_server
